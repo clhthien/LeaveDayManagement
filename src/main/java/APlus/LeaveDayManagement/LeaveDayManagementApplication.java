33071c3e0c0c7aae5e2c4a836f84f4c79fd28bb6
clhthien <thien.caolehoan@hcmut.edu.vn>
2025-03-22 09:32:02 +0700

    first commit

---
+++ src/main/java/APlus/LeaveDayManagement/LeaveDayManagementApplication.java
@@ -0,0 +1,13 @@
+package APlus.LeaveDayManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeaveDayManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeaveDayManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/APlus/LeaveDayManagement/LeaveDayManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package APlus.LeaveDayManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeaveDayManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
