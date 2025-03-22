8cd11613b56ec58f0c3b867745769bd24ffc40d8
Andy <andybk.nguyen1202@gmail.com>
2025-03-22 10:44:25 +0700

    add user mapper

---
+++ src/main/java/APlus/LeaveDayManagement/mapper/UserMapper.java
@@ -0,0 +1,11 @@
+package APlus.LeaveDayManagement.mapper;
+
+import APlus.LeaveDayManagement.model.User;
+import APlus.LeaveDayManagement.response.UserDTO.UserDTO;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+public abstract class UserMapper {
+    public abstract UserDTO toUserDTO(User user);
+    public abstract User touser(UserDTO userDTO);
+}
