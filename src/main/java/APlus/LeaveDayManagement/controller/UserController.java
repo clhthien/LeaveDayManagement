f7c9f47ea118f1353e881b125d87b943271c9bd9
Andy <andybk.nguyen1202@gmail.com>
2025-03-22 10:40:18 +0700

    add update user feature

--- src/main/java/APlus/LeaveDayManagement/controller/UserController.java
+++ src/main/java/APlus/LeaveDayManagement/controller/UserController.java
@@ -1,9 +1,11 @@
 package APlus.LeaveDayManagement.controller;
 
+import APlus.LeaveDayManagement.model.User;
 import APlus.LeaveDayManagement.response.ApiResponse;
 import APlus.LeaveDayManagement.response.UserDTO.UserDTO;
 import APlus.LeaveDayManagement.service.inter.IUserService;
 import lombok.RequiredArgsConstructor;
+import org.apache.coyote.Response;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,4 +21,8 @@ public class UserController {
         return ResponseEntity.status(response.getStatus()).body(response);
     }
 
+    public ResponseEntity<ApiResponse<User>> updateUser(UserDTO userDTO){
+        ApiResponse<User> response = userService.updateUser(userDTO);
+        return ResponseEntity.status(response.getStatus()).body(response);
+    }
 }
--- src/main/java/APlus/LeaveDayManagement/service/inter/IUserService.java
+++ src/main/java/APlus/LeaveDayManagement/service/inter/IUserService.java
@@ -1,8 +1,11 @@
 package APlus.LeaveDayManagement.service.inter;
 
+import APlus.LeaveDayManagement.model.User;
 import APlus.LeaveDayManagement.response.ApiResponse;
 import APlus.LeaveDayManagement.response.UserDTO.UserDTO;
 
 public interface IUserService {
     public ApiResponse addUser(UserDTO request);
+
+    public ApiResponse<User> updateUser(UserDTO userDTO);
 }
